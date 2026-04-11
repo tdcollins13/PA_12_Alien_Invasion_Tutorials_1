0156e11235a1005959b602ed1744f42567753550
Tyler Collins <tdcollins13@gmail.com>
2026-04-11 13:47:52 -0400

    Change game screen size to improve viewability

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings:
     def __init__(self):
         self.name: str = "Alien Invasion"
         self.screen_w = 1200
-        self.screen_h = 800
+        self.screen_h = 600
         self.FPS = 60
         self.bg_file = Path.cwd() / 'Assets'/ 'images' / 'Starbasesnow.png'
 
