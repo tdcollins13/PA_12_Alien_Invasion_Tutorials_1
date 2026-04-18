da104edbf0cd77adb60b10567d16bce6f9f45b42
Tyler Collins <tdcollins13@gmail.com>
2026-04-18 14:10:27 -0400

    p2 - Finish fleet reacting to boundaries and shifting downward

--- alien_fleet.py
+++ alien_fleet.py
@@ -63,6 +63,22 @@ class AlienFleet:
         new_alien = Alien(self, current_x, current_y)
         self.fleet.add(new_alien)
 
+    def _check_fleet_edges(self):
+        alien: 'Alien'
+        for alien in self.fleet:
+            if alien.check_edges():
+                self._drop_alien_fleet()
+                self.fleet_direction *= -1
+                break
+    
+    def _drop_alien_fleet(self):
+        for alien in self.fleet:
+            alien.y += self.fleet_drop_speed
+
+    def update_fleet(self):
+        self._check_fleet_edges()
+        self.fleet.update()
+
     def draw(self):
         alien: 'Alien'
         for alien in self.fleet:
