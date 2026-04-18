b7f7e258ef021f87851f6da2c3294cf6657d9a99
Tyler Collins <tdcollins13@gmail.com>
2026-04-18 12:28:13 -0400

    p2 - Create a single row of stationary aliens for alien fleet

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,7 +3,8 @@ import pygame
 from settings import Settings
 from ship import Ship
 from arsenal import Arsenal
-from alien import Alien
+# from alien import Alien
+from alien_fleet import AlienFleet
 
 class AlienInvasion:
 
@@ -30,21 +31,22 @@ class AlienInvasion:
 
         # Create ship
         self.ship = Ship(self, Arsenal(self))
-        self.alien = Alien(self, 10, 10)
+        self.alien_fleet = AlienFleet(self)
+        self.alien_fleet.create_fleet()
     
     def run_game(self):
         #Game loop
         while self.running:
             self._check_events()
             self.ship.update()
-            self.alien.update()
+            #self.alien.update()
             self._update_screen()
             self.clock.tick(self.settings.FPS)
 
     def _update_screen(self):
         self.screen.blit(self.bg, (0,0))
         self.ship.draw()
-        self.alien.draw_alien()
+        self.alien_fleet.draw()
         pygame.display.flip()
 
     def _check_events(self):
