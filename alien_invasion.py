c4846073a63659135305d1d81a4ccbe9755579f4
Tyler Collins <tdcollins13@gmail.com>
2026-04-18 15:08:47 -0400

    p2 - Set up Alien Fleet for sound and collisions with projectiles and bottom of game screen

--- alien_invasion.py
+++ alien_invasion.py
@@ -53,7 +53,15 @@ class AlienInvasion:
             self._reset_level()
             # subtract 1 life if possible
 
-        
+        # check collisions for aliens and bottom of screen
+        if self.alien_fleet.check_fleet_bottom():
+            self._reset_level()
+
+        # check collisions for projectiles and aliens
+        collisions = self.alien_fleet.check_collisions(self.ship.arsenal.arsenal)
+        if collisions:
+            self.impact_sound.play()
+            self.impact_sound.fadeout(500)
 
     def _reset_level(self):
         self.ship.arsenal.arsenal.empty()
--- settings.py
+++ settings.py
@@ -15,6 +15,7 @@ class Settings:
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
+        self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
         self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
@@ -23,6 +24,6 @@ class Settings:
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_w = 30
         self.alien_h = 30
-        self.fleet_speed = 2
+        self.fleet_speed = 1
         self.fleet_direction = 1
         self.fleet_drop_speed = 40
