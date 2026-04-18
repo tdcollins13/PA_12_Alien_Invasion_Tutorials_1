c6c40566092c0515ca3d5cde3872e5cd37f7ce33
Tyler Collins <tdcollins13@gmail.com>
2026-04-18 11:18:38 -0400

    p2 - have aliens drop while hitting edges

--- alien.py
+++ alien.py
@@ -31,12 +31,14 @@ class Alien(Sprite):
         # flips direction
         if self.check_edges():
             self.settings.fleet_direction *= -1
+            self.y += self.settings.fleet_drop_speed
 
         self.x += temp_speed * self.settings.fleet_direction
         self.rect.x = self.x
+        self.rect.y = self.y
 
     def check_edges(self):
-        return (self.rect.right >= self.boundaries.right or self.rect.left <= self.boundaries.left) 
+        return (self.rect.right >= self.boundaries.right or self.rect.left <= self.boundaries.left)
 
 
     def draw_alien(self):
