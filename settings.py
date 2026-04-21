be91d1c294b16e24b965351311ee9aa0de60d485
Tyler Collins <tdcollins13@gmail.com>
2026-04-21 19:39:54 -0400

    p3 - Split settings into dynamic and static, add increase difficulty function & scale

--- settings.py
+++ settings.py
@@ -11,21 +11,16 @@ class Settings:
         self.ship_file = Path.cwd() / 'Assets' / 'images' / 'ship2(no bg).png'
         self.ship_w = 40
         self.ship_h = 60
-        self.ship_speed = 5
-        self.starting_ship_count = 3
 
         self.bullet_file = Path.cwd() / 'Assets' / 'images' / 'laserBlast.png'
         self.laser_sound = Path.cwd() / 'Assets' / 'sound' / 'laser.mp3'
         self.impact_sound = Path.cwd() / 'Assets' / 'sound' / 'impactSound.mp3'
-        self.bullet_speed = 7
         self.bullet_w = 25
         self.bullet_h = 80
-        self.bullet_amount = 5
 
         self.alien_file = Path.cwd() / 'Assets' / 'images' / 'enemy_4.png'
         self.alien_w = 30
         self.alien_h = 30
-        self.fleet_speed = 1
         self.fleet_direction = 1
         self.fleet_drop_speed = 40
 
@@ -37,3 +32,21 @@ class Settings:
         self.button_font_size = 48
         self.HUD_font_size = 20
         self.font_file = Path.cwd() / 'Assets' / 'Fonts' / 'Silkscreen' / 'Silkscreen-Bold.ttf'
+
+        self.difficulty_scale = 1.1
+
+    def initialize_dynamic_settings(self):
+        self.ship_speed = 5
+        self.starting_ship_count = 3
+        self.bullet_w = 25
+        self.bullet_h = 80
+        self.bullet_speed = 7
+        self.bullet_amount = 5
+        self.fleet_speed = 1
+        self.fleet_drop_speed = 40
+
+    def increase_difficulty(self):
+        # difficulty scale
+        self.ship_speed *= self.difficulty_scale
+        self.bullet_speed *= self.difficulty_scale
+        self.fleet_speed *= self.difficulty_scale
