cb5f01ca86b87181106c8a68669bb029d7529f5c
Tyler Collins <tdcollins13@gmail.com>
2026-04-22 16:09:43 -0400

    p3 - Finished up HUD to display lives and levels and plugged into alien invasion game

--- alien_invasion.py
+++ alien_invasion.py
@@ -79,7 +79,7 @@ class AlienInvasion:
             self._reset_level()
             self.settings.increase_difficulty()
             self.game_stats.update_level()
-            # Update HUD view
+            self.HUD.update_level()
 
     def _check_game_status(self):
         if self.game_stats.ships_left > 0:
